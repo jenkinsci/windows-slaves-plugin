dfda230d61f7ca9727881880dd1460a8f46439f0
René Scheibe <rene.scheibe@gmail.com>
2018-10-27 12:23:38 +0200

    Replace manual resource cleanup with try-with-resources construct
    Removing the "throws IOException" as this is caught.

--- src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
+++ src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
@@ -429,16 +429,12 @@ public class ManagedWindowsServiceLauncher extends ComputerLauncher {
         return evnp.getEnvVars();
     }
 
-
-    private void checkPort135Access(PrintStream logger, String name, InetAddress host) throws IOException {
-        Socket s = new Socket();
-        try {
-            s.connect(new InetSocketAddress(host,135),5000);
+    private void checkPort135Access(PrintStream logger, String name, InetAddress host) {
+        try (Socket s = new Socket()) {
+            s.connect(new InetSocketAddress(host, 135), 5000);
         } catch (IOException e) {
-            logger.println("Failed to connect to port 135 of "+name+". Is Windows firewall blocking this port? Or did you disable DCOM service?");
+            logger.println("Failed to connect to port 135 of " + name + ". Is Windows firewall blocking this port? Or did you disable DCOM service?");
             // again, let it continue.
-        } finally {
-            s.close();
         }
     }
 
